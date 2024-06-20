dc4d4a34e9d465f2d7b7b690f1b9a26cff7b5e08
Alaa Muhissen <alaa.muhissen1912@gmail.com>
2024-06-20 12:34:13 +0300

    Add all the categories and subCategories to the schema

--- models/closet.schema.js
+++ models/closet.schema.js
@@ -52,14 +52,139 @@ const categoriesSchema = new mongoose.Schema({
         T_Shirts: {
             type: Map,
             of: itemSchema
+        },
+        Hoodies: {
+            type: Map,
+            of: itemSchema
+        },
+        Blouses: {
+            type: Map,
+            of: itemSchema
+        },
+       
+        Shirts: {
+            type: Map,
+            of: itemSchema
+        },
+        Sweaters: {
+            type: Map,
+            of: itemSchema
+        },
+        Basics: {
+            type: Map,
+            of: itemSchema
         }
     },
     Bottoms: {
         Jeans: {
             type: Map,
             of: itemSchema
+        },
+        Skirts: {
+            type: Map,
+            of: itemSchema
+        },
+        Shorts: {
+            type: Map,
+            of: itemSchema
         }
-    }
+    },
+    Outwear: {
+        Jackets: {
+            type: Map,
+            of: itemSchema
+        },
+        Coats: {
+            type: Map,
+            of: itemSchema
+        },
+        Vests: {
+            type: Map,
+            of: itemSchema
+        }
+    },
+    Shoes: {
+        Casual_Shoes: {
+            type: Map,
+            of: itemSchema
+        },
+        Formal_Shoes: {
+            type: Map,
+            of: itemSchema
+        },
+        Boots: {
+            type: Map,
+            of: itemSchema
+        },
+        Sandals_and_Flip_Flops: {
+            type: Map,
+            of: itemSchema
+        },
+        Dress_Shoes: {
+            type: Map,
+            of: itemSchema
+        }
+    },
+    Bags: {
+        Shoulder_Bag: {
+            type: Map,
+            of: itemSchema
+        },
+        Crossbody_Bag: {
+            type: Map,
+            of: itemSchema
+        },
+        Backpack: {
+            type: Map,
+            of: itemSchema
+        },
+        Wallet: {
+            type: Map,
+            of: itemSchema
+        },
+        Beach_Bag: {
+            type: Map,
+            of: itemSchema
+        },
+        Laptop_Bag: {
+            type: Map,
+            of: itemSchema
+        }
+    },
+    Head_wear: {
+        Hats: {
+            type: Map,
+            of: itemSchema
+        },
+        Hejab: {
+            type: Map,
+            of: itemSchema
+        }
+    },
+    Jewelry: {
+        Necklaces: {
+            type: Map,
+            of: itemSchema
+        },
+        Earrings: {
+            type: Map,
+            of: itemSchema
+        },
+        Rings: {
+            type: Map,
+            of: itemSchema
+        },
+        Watches: {
+            type: Map,
+            of: itemSchema
+        }
+    },
+    Other_items: {
+        others: {
+            type: Map,
+            of: itemSchema
+        } 
+    },
 });
 
 // Outfits schema
